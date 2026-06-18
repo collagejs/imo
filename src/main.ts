d631161a980f491c8ea96a71451d9426a3bd441c
Ramirez Vargas, José Pablo <webJose@gmail.com>
2026-06-17 22:01:16 -0600

    fix: Correct IMO UI options

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { mount } from 'svelte';
 import App from './App.svelte';
-import { getInitialImoUiOptions } from './shared/options.js';
+import { getInitialImoUiFactoryOptions } from './shared/options.js';
 import { initImoUiOptions } from './lib/state/imoUiOptions.js';
 
-initImoUiOptions(await getInitialImoUiOptions());
+initImoUiOptions((await getInitialImoUiFactoryOptions()).ui);
 
 const app = mount(App, {
   target: document.getElementById('app')!,
