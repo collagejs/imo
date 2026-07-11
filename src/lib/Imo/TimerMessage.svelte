6fc5bb9744c08aeda5d2fa73b33319d65ea51630
Ramirez Vargas, José Pablo <webJose@gmail.com>
2026-07-10 20:49:22 -0600

    fix: Use onMount() to set up interval in TimerMessage so as to clean up the interval on unmount

--- src/lib/Imo/TimerMessage.svelte
+++ src/lib/Imo/TimerMessage.svelte
@@ -1,27 +1,31 @@
 <script lang="ts">
-    import type { Snippet } from "svelte";
+    import { onMount, type Snippet } from "svelte";
     import type { HTMLAttributes } from "svelte/elements";
 
-    type Props = Omit<HTMLAttributes<HTMLDivElement>, 'children'> & {
+    type Props = Omit<HTMLAttributes<HTMLDivElement>, "children"> & {
         time: number;
         children?: Snippet<[number]>;
         stopped?: Snippet;
         onTimer?: () => void;
-    }
+    };
 
     let { time, children, stopped, onTimer, ...restProps }: Props = $props();
 
     let timerStopped = $state(false);
     // svelte-ignore state_referenced_locally
     let timeLeft = $state(time);
+    let timer: ReturnType<typeof setTimeout>;
 
-    const timer = setInterval(() => {
-        timeLeft -= 1;
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            onTimer?.();
-        }
-    }, 1000);
+    onMount(() => {
+        timer = setInterval(() => {
+            timeLeft -= 1;
+            if (timeLeft <= 0) {
+                clearInterval(timer);
+                onTimer?.();
+            }
+        }, 1000);
+        return () => clearInterval(timer);
+    });
 
     export function stop() {
         clearInterval(timer);
